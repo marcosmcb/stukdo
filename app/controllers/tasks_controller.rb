660494899b374131d35340d95ff1c24d09365390
Marcos <marcos.bcc2011@gmail.com>
2015-08-16 22:24:41 -0300

    Added change task custom action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy, :change]
 
   respond_to :html
 
@@ -40,6 +40,10 @@ class TasksController < ApplicationController
   end
 
   def change
+    @task.update_attributes(state: params[:state])
+    respond_to do |format|
+      format.html {redirect_to tasks_path, notice: "Task Update"}
+    end
   end
 
 
