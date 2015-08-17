be85cd8f5dc30890d91000130dc622700c5835a0
Marcos <marcos.bcc2011@gmail.com>
2015-08-16 22:59:52 -0300

    Made test page with task panel

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Stukdo::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pages#home'
   get 'about' => 'pages#about'
+  get 'test' => "pages#test"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
